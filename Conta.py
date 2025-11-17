613963eee458a47840aca8f1b1d9b4421fb215e9
Anderson Duarte <57947382+OverclockerBR@users.noreply.github.com>
2025-11-17 05:56:37 +0000

    Transferir_saldo e saldo_transferido criados

--- Conta.py
+++ Conta.py
@@ -2,7 +2,7 @@ import datetime
 
 class Conta:
 
-    def __init__(self, cliente, numero, saldo):     # Parametros para o Construtor da classe
+    def __init__(self, cliente, numero, saldo):     # Parametros para o Construtor da classe Conta
         self.cliente = cliente      # atribuir Nome ao cliente
         self.numero = numero        # atribuir Numero da conta
         self.saldo = saldo      # atribuir Saldo inicial da conta
@@ -14,11 +14,11 @@ class Conta:
         print(f"\nDepósito de R${valor} realizado com sucesso.")
         print(f"Saldo atual é de: R${self.saldo}")
 
-
+    
     def sacar(self, valor):
         if self.saldo < valor:
             print("\nSaldo insuficiente para esse valor\n".upper())     # Fazendo ficar em maiusculo a mensagem de saldo insuficiente
-            print(f"O saldo atual continua sendo de R${self.saldo}\n".upper())
+            print(f"O saldo atual da sua conta continua sendo de R${self.saldo}\n".upper())
         else:
             self.saldo -= valor
             print(f"\nSaque de R${valor} efetuado com sucesso.")
@@ -30,33 +30,52 @@ class Conta:
         print(f"\n{32*"="} Extrato da conta {32*"="}".upper())
         print(f"Cliente: {self.cliente}")
         print(f"Numero da conta: {self.numero}")
-        print(f"saldo atual: R${self.saldo}\n\n")
+        print(f"saldo atual: R${self.saldo}\n")
 
 
     def transferir_saldo(self, conta_destino, valor):
-        pass
-
-
+        if self.saldo <  valor:
+            print(f"Saldo insuficiente para essa transferencia.\n".upper())
+            print(f"O saldo atual da sua conta continua sendo de R${self.saldo}\n".upper())
+        else:
+            conta_destino.saldo_transferido(valor)
+            self.saldo -= valor
+            
+            
+    def saldo_transferido(self, valor):     # Metodo para receber o valor transferido
+        self.saldo += valor        # ou poderia ser também self.saldo = self.saldo + valor
+        print(f"\nTransferencia de R${valor} para {self.cliente} realizado com sucesso.")
+        #print(f"Saldo atual é de: R${self.saldo}\n")
 
 
 
 ############################## TESTE DA CLASSE CONTA ###############################
-"""c1 = Conta("Ana Maria", 12345, 10000)
-print(f"Nome do(a) cliente: {c1.cliente}")
-print(f"Numero da conta: {c1.numero}")
-print(f"Saldo atual: {c1.saldo}")
-print(f"Data de abertura da conta: {c1.data_abertura}")
-
-c1.depositar(900)
-
-c1.sacar(5000)
-
-c1.gerar_extrato()"""
-
 conta1 = Conta("João Silva", 67890, 5000)
 conta2 = Conta("Maria Oliveira", 54321, 15000)
 
-if conta1 == conta2:
+print(f"\nNome do(a) cliente: {conta1.cliente}")
+print(f"Numero da conta: {conta1.numero}")
+print(f"Saldo atual: {conta1.saldo}")
+print(f"Data de abertura da conta: {conta1.data_abertura}\n\n")
+
+
+print(f"Nome do(a) cliente: {conta2.cliente}")
+print(f"Numero da conta: {conta2.numero}")
+print(f"Saldo atual: {conta2.saldo}")
+print(f"Data de abertura da conta: {conta2.data_abertura}\n\n")
+
+#c1.depositar(900)
+
+#c1.sacar(5000)
+
+#c1.gerar_extrato()
+
+"""if conta1 == conta2:
     print("\nVocê não pode transferir para essa mesma conta.\n".upper())
 else:
-    print("\nTransferencia permitida.\n")
+    print("\nTransferencia permitida.\n\n")"""
+
+print(conta1.transferir_saldo(conta2, 5000))
+
+print(conta2.gerar_extrato())
+print(conta1.gerar_extrato())
